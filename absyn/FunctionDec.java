0f22edc1e4fef83c46e7a55b5509624b08792e4e
AlexLapena <alapena@mail.uoguelph.ca>
2017-03-06 17:56:07 -0500

    Grammar Work

--- absyn/FunctionDec.java
+++ absyn/FunctionDec.java
@@ -3,11 +3,13 @@ package absyn;
 public class FunctionDec extends Dec {
   public String func;
   public NameTy result;
-  public ExpList exps;
-  public FunctionDec( int pos, NameTy result, String func, ExpList exps ) {
+  public VarDecList params;
+  public CompoundExp body;
+  public FunctionDec( int pos, NameTy result, String func, VarDecList params, CompoundExp body) {
     this.pos = pos;
     this.result = result;
     this.func = func;
-    this.exps = exps;
+    this.params = params;
+    this.body = body;
   }
 }
--- absyn/VoidExp.java
+++
@@ -1,9 +0,0 @@
-package absyn;
-
-public class VoidExp extends Exp {
-  public Exp output;
-  public VoidExp( int pos, Exp output ) {
-    this.pos = pos;
-    this.output = output;
-  }
-}
