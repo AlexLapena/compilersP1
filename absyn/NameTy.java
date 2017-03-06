8e8668563803f8d202359d5c212ed083e2744019
AlexLapena <alapena@mail.uoguelph.ca>
2017-03-06 15:17:34 -0500

    constants added

--- absyn/NameTy.java
+++ absyn/NameTy.java
@@ -2,6 +2,8 @@ package absyn;
 
 //Concrete class --> needs to be examined
 public class NameTy extends absyn implements VarDec{
+  public static int INT = 0;
+  public static int VOID = 1;
   public NameTy( int pos, int typ) {
   	this.pos = pos;
   	this.typ = typ;
